fafb9699e15d0197a7611f7389e6c7690584fbc3
Alexg2195 <alexg2195@gmail.com>
2016-04-11 16:49:52 -0400

    omitting files that are too large without stream till later

--- insertData.js
+++ insertData.js
@@ -9,21 +9,30 @@ var db = mongoose.connection;
 
 var personData = require(__dirname + '/models/dataPt.js');
 
-var pathToData = 'C:/Users/Alexg2195/Desktop/HackathonV12/data/NJ/';
+var pathToData = 'C:/Users/Alexg2195/Desktop/HackathonV12/data/NY/';
+
+MaxFileSize = 268435440; // In Bytes
 
 fs.readdir(pathToData, function(err,files){
   console.log("Setting up files to be loaded");
   if(err) throw err;
   files.forEach(function(file){
     calls.push(function(callback) {
-      var obj = JSON.parse(fs.readFileSync(pathToData + file, 'utf8'));
-      personData.collection.insertMany(obj, function(err) {
-        if (err) {
-          callback(err);
-        }
-        console.log(file + " Inserted!");
-        callback(null, file);
-      });
+      var stats = fs.statSync(pathToData + file);
+      var fileSizeInBytes = stats["size"];
+      if (fileSizeInBytes < MaxFileSize) {
+        var obj = JSON.parse(fs.readFileSync(pathToData + file, 'utf8'));
+        personData.collection.insertMany(obj, function(err) {
+          if (err) {
+            callback(err);
+          }
+          console.log(file + " Inserted!");
+          callback(null, file);
+        });
+      } else {
+        console.log("File '" + file + "' is too large! Will not be inserted...");
+        callback(null, null);
+      }
     });
   });
 

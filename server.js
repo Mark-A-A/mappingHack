70dffa60e7252929785c853682a47104e222efa6
Alexg2195 <alexg2195@gmail.com>
2016-04-11 04:44:47 -0400

    all fields working properly yayyyy

--- server.js
+++ server.js
@@ -43,17 +43,38 @@ app.post("/search", function(req, res){
 
   for (var key in formInput) {
     if (formInput.hasOwnProperty(key)) {
-      switch (key) {
+      console.log("adding the following key to search params: " + key);
+      console.log(formInput[key]);
+      switch (formInput[key].name) {
         case "Age":
-          if (formInput[key] !== "Age Group") {
-            var rangeLow = formInput[key].slice(0,2);
-            var rangeHi = formInput[key].slice(3,5);
+          if (formInput[key].value !== "Age Group") {
+            var rangeLow = formInput[key].value.slice(0,2);
+            var rangeHi = formInput[key].value.slice(3,5);
             searchParams.push({"Age": { $gte: rangeLow, $lte: rangeHi }});
           }
           break;
         case "Gender":
-          if (formInput[key] === "M" || formInput[key] === "F") {
-            searchParams.push({"Gender": formInput[key]});
+          if (formInput[key].value === "M" || formInput[key].value === "F") {
+            searchParams.push({"Gender": formInput[key].value});
+          }
+          break;
+        case "EducationLevel":
+          switch (formInput[key].value) {
+            case "High School":
+              searchParams.push({"EducationLevel": 1});
+              break;
+            case "Some College":
+              searchParams.push({"EducationLevel": 2});
+              break;
+            case "Completed College":
+              searchParams.push({"EducationLevel": 3});
+              break;
+            case "Graduate School":
+              searchParams.push({"EducationLevel": 4});
+              break;
+            default:
+              // statements_def
+              break;
           }
           break;
         default:
@@ -63,8 +84,9 @@ app.post("/search", function(req, res){
     }
   }
 
-  var query = Data.find({ $and: searchParams }).limit(1500);
-
+  var query = Data.find({ $and: searchParams }).limit(20000);
+  console.log("Search params are %s", searchParams);
+  console.log(searchParams);
   query.exec(function (err, data) {
     if (err) {
       throw err;
